b0fe91c446ac05c1a1d0f78ad3e3f56c0b091f4b
mrey <mrey@teltek.es>
2015-08-03 16:54:42 +0200

    Upload only AUTONOMOUS videos

--- Command/YoutubeUploadCommand.php
+++ Command/YoutubeUploadCommand.php
@@ -114,6 +114,7 @@ EOT
           ->field('status')->equals(MultimediaObject::STATUS_PUBLISHED)
           ->field('broadcast')->references($publicBroadcast)
           /* ->field('tags.cod')->equals('IMPORTANT') TODO When Tag with code 'IMPORTANT' is done ('autónomo' in Pumukit1.8) */
+          ->field('tags.cod')->equals('PUDEAUTO')
           ->field('properties.youtube')->exists(false)
           ->getQuery()
           ->execute();
