e6b5e71b27cb00d644b38fbacc33117f3e17f51e
mrey <mrey@teltek.es>
2015-08-03 17:04:37 +0200

    Redefined youtube logger

--- DependencyInjection/PumukitYoutubeExtension.php
+++ DependencyInjection/PumukitYoutubeExtension.php
@@ -6,14 +6,33 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class PumukitYoutubeExtension extends Extension
+class PumukitYoutubeExtension extends Extension implements PrependExtensionInterface
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        $container->prependExtensionConfig('monolog', array(
+                                                            'channels' => array('youtube'),
+                                                            'handlers' => array(
+                                                                                'youtube' => array(
+                                                                                                   'type' => 'stream',
+                                                                                                   'path' => "%kernel.logs_dir%/youtube_%kernel.environment%.log",
+                                                                                                   'level' => 'info',
+                                                                                                   'channels' => array('youtube')
+                                                                                                   )
+                                                                                )
+                                                            ));
+    }
+
     /**
      * {@inheritdoc}
      */
