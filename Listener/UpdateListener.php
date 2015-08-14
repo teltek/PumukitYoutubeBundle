db948913818fc03c3d99f97f84bd126482c6fd60
mrey <mrey@teltek.es>
2015-08-14 16:36:53 +0200

    Added Update Listener

---
+++ Listener/UpdateListener.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Pumukit\YoutubeBundle\Listener;
+
+use Doctrine\ODM\MongoDB\Event\LifecycleEventArgs;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Pumukit\YoutubeBundle\Document\Youtube;
+use Pumukit\SchemaBundle\Document\MultimediaObject;
+
+class UpdateListener
+{
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function preRemove(LifecycleEventArgs $args)
+    {
+        $document = $args->getDocument();
+
+        if ($document instanceof MultimediaObject) {
+            $dm = $this->container->get("doctrine_mongodb.odm.document_manager");
+            $youtubeRepo = $dm->getRepository("PumukitYoutubeBundle:Youtube");
+            $youtube = $youtubeRepo->createQueryBuilder()
+                ->field('multimediaObjectId')->equals($document->getId())
+                ->getQuery()
+                ->getSingleResult();
+
+            if (null !== $youtube) {
+                $youtube->setMultimediaObjectUpdateDate(new \DateTime('now'));
+                $dm->persist($youtube);
+                $dm->flush();
+            }
+        }
+    }
+}
