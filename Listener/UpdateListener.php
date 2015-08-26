3ac837d5633e16d1985137fa5b93f59399fbd488
mrey <mrey@teltek.es>
2015-08-26 10:46:12 +0200

    Revert "Revert "#7651 Improved UpdateListener to only listen on multimedia object update event""
    This reverts commit 2ead24b7a182c103fced7ee6937abe50c3bbf8aa.

--- Listener/UpdateListener.php
+++ Listener/UpdateListener.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Pumukit\YoutubeBundle\Document\Youtube;
 use Pumukit\SchemaBundle\Document\MultimediaObject;
+use Pumukit\SchemaBundle\Event\MultimediaObjectEvent;
 
 class UpdateListener
 {
@@ -17,24 +18,22 @@ class UpdateListener
         $this->container = $container;
     }
 
-    public function postUpdate(LifecycleEventArgs $args)
+    public function onMultimediaObjectUpdate(MultimediaObjectEvent $event)
     {
-        $document = $args->getDocument();
+        $document = $event->getMultimediaObject();
 
-        if ($document instanceof MultimediaObject) {
-            $dm = $this->container->get("doctrine_mongodb.odm.document_manager");
-            $youtubeRepo = $dm->getRepository("PumukitYoutubeBundle:Youtube");
-            $youtube = $youtubeRepo->createQueryBuilder()
-                ->field('multimediaObjectId')->equals($document->getId())
-                ->getQuery()
-                ->getSingleResult();
+        $dm = $this->container->get("doctrine_mongodb.odm.document_manager");
+        $youtubeRepo = $dm->getRepository("PumukitYoutubeBundle:Youtube");
+        $youtube = $youtubeRepo->createQueryBuilder()
+            ->field('multimediaObjectId')->equals($document->getId())
+            ->getQuery()
+            ->getSingleResult();
 
-            if (null !== $youtube) {
-                if ($youtube->getMultimediaObjectUpdateDate() < $youtube->getSyncMetadataDate()) {
-                    $youtube->setMultimediaObjectUpdateDate(new \DateTime('now'));
-                    $dm->persist($youtube);
-                    $dm->flush();
-                }
+        if (null !== $youtube) {
+            if ($youtube->getMultimediaObjectUpdateDate() < $youtube->getSyncMetadataDate()) {
+                $youtube->setMultimediaObjectUpdateDate(new \DateTime('now'));
+                $dm->persist($youtube);
+                $dm->flush();
             }
         }
     }
