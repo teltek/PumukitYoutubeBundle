992775eef47576c3fc71efb75d07c507e4d0d8dc
Alfro <arodriguez@teltek.es>
2015-10-06 09:45:29 +0200

    Added new Modal window with information.

--- Controller/ModalController.php
+++ Controller/ModalController.php
@@ -5,46 +5,47 @@ namespace Pumukit\YoutubeBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\EventDispatcher\Event;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Pumukit\SchemaBundle\Document\Tag;
-use Pumukit\NewAdminBundle\Form\Type\TagType;
-
+use Pumukit\SchemaBundle\Document\MultimediaObject;
+use Pumukit\YoutubeBundle\Document\Youtube;
 
 class ModalController extends Controller
 {
-    
     /**
-     * 
-     * pumukitnewadmin_tag_create
+     * @Route ("/modal/mm/{id}", name="pumukityoutube_modal_index")
      * @Template()
      */
-    
-    public function indexAction(Request $request)
-    {        
+    public function indexAction(Request $request, MultimediaObject $mm)
+    {
         $dm = $this->get('doctrine_mongodb')->getManager();
-        $repo = $dm->getRepository('PumukitSchemaBundle:Tag');
+        $youtubeRepo = $dm->getRepository('PumukitYoutubeBundle:Youtube');
+        $youtube = $youtubeRepo->find($mm->getProperty('youtube'));
 
-        $tag = new Tag();
-
-        $translator = $this->get('translator');
-        $locale = $request->getLocale();
-
-        $form = $this->createForm(new TagType($translator, $locale), $tag);
-
-        if (($request->isMethod('PUT') || $request->isMethod('POST')) && $form->bind($request)->isValid()) {
-            try {
-                $dm->persist($tag);
-                $dm->flush();
-            } catch (\Exception $e) {
-                return new JsonResponse(array("status" => $e->getMessage()), 409);
-            }
-
-            return $this->redirect($this->generateUrl('pumukitnewadmin_tag_list'));
+        $youtubeStatus = 'none';
+        switch ($youtube->getStatus()) {
+            case Youtube::STATUS_DEFAULT:
+            case Youtube::STATUS_UPLOADING:
+            case Youtube::STATUS_PROCESSING:
+            $youtubeStatus = 'proccessing';
+            break;
+            case Youtube::STATUS_PUBLISHED:
+            $youtubeStatus = 'published';
+            break;
+            case Youtube::STATUS_HTTP_ERROR:
+            case Youtube::STATUS_ERROR:
+            case Youtube::STATUS_UPDATE_ERROR:
+            case Youtube::STATUS_DUPLICATED:
+            $youtubeStatus = 'error';
+            break;
+            case Youtube::STATUS_REMOVED:
+            $youtubeStatus = 'removed';
+            break;
+            case Youtube::STATUS_NOTIFIED_ERROR:
+            case Youtube::STATUS_TO_DELETE:
+            $youtubeStatus = 'caca?';
+            break;
         }
-       
-        return array('tag' => $tag, 'form' => $form->createView());
-    }
 
+        return array('mm' => $mm, 'youtube' => $youtube, 'youtube_status' => $youtubeStatus);
+    }
 }
