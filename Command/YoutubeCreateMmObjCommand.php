3cb62bbb7c47177d64e522970593ca0c7d958f0b
Ruben Gonzalez <rubenrua@gmail.com>
2016-10-16 20:27:06 +0200

    1st version of YoutubeCreateMmObjCommand.php

---
+++ Command/YoutubeCreateMmObjCommand.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace Pumukit\YoutubeBundle\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Pumukit\SchemaBundle\Document\Tag;
+use Pumukit\SchemaBundle\Document\MultimediaObject;
+use Pumukit\SchemaBundle\Document\Series;
+use Pumukit\YoutubeBundle\Document\Youtube;
+
+class YoutubeCreateMmObjCommand extends ContainerAwareCommand
+{
+    private $dm = null;
+    private $tagRepo = null;
+    private $mmobjRepo = null;
+    private $seriesRepo = null;
+    private $youtubeRepo = null;
+
+    private $youtubeService;
+    private $factoryService;
+    private $logger;
+
+    protected function configure()
+    {
+        $this
+            ->setName('youtube:createmmobj')
+            ->setDescription('Create a multimedia object from Youtube')
+            ->addArgument('yid', InputArgument::REQUIRED, 'YouTube ID')
+            ->addArgument('series', InputArgument::OPTIONAL, 'Series id where the object is created')
+            ->addOption('step', 'S', InputOption::VALUE_REQUIRED, 'Step of the importation. See help for more info', -99)
+            ->setHelp(<<<EOT
+Command to create a multimedia object from Youtube.
+
+Steps:
+ * 1.- Create the Multimedia Object.
+ * 2.- Download the image
+ * 3.- Download/move the tracks
+
+Example:
+  <info>php bin/console youtube:createmmobj --env=prod --step=1 XXXXXYYYY</info>
+
+EOT
+          );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->initParameters();
+
+        $yid = $input->getArgument('yid');
+        $step = $input->getOption('step');
+        switch ($step) {
+        case 1:
+            $series = $this->getSeries($input->getArgument('series'));
+            $output->writeln(' * Creating multimedia object from id . ' . $yid);
+            $this->createMultimediaObject($yid, $series, $output);
+            break;
+        case 2:
+            $output->writeln(' * TODO ');
+            break;
+        case 3:
+            $output->writeln(' * TODO ');
+            break;
+        default:
+            $output->writeln('<error>Select a valid step</error>');
+        }
+    }
+
+
+    private function createMultimediaObject($yid, Series $series, OutputInterface $output)
+    {
+        try {
+            $meta = $this->youtubeService->getVideoMeta($yid);
+        } catch (\Exception $e) {
+            $output->writeln('<error>No Youtube video with id ' . $yid .'</error>');
+            return false;
+        }
+
+        //Check if exists
+        if ($this->getMmObjFromYid($yid)) {
+            $output->writeln('<error>Already exists a mmobj from Youtube video with id ' . $yid .'</error>');
+            return false;
+        }
+
+        //Create using the factory
+        $mmobj = $this->factoryService->createMultimediaObject($series, false);
+        $mmobj->setTitle($meta['out']['snippet']['title']);
+        $mmobj->setDescription($meta['out']['snippet']['description']);
+        $mmobj->setKeywords($meta['out']['snippet']['tags']);
+        $dataTime = \DateTime::createFromFormat('Y-m-d\TH:i:s', substr($meta['out']['snippet']['publishedAt'], 0, 19));
+        $mmobj->setRecordDate($dataTime);
+        $mmobj->setPublicDate($dataTime);
+        $mmobj->setProperty('origin', 'youtube');
+        $mmobj->setProperty('youtubemeta', $meta['out']);
+
+        $this->dm->persist($mmobj);
+        $this->dm->flush();
+    }
+
+
+
+    private function getMmObjFromYid($yid)
+    {
+        $mmobj = $this->mmobjRepo->findOneBy(array('properties.youtubemeta.id' => $yid));
+        if ($mmobj) {
+            return $mmobj;
+        }
+
+
+        $yt = $this->youtubeRepo
+            ->createQueryBuilder()
+            ->field('youtubeId')->equals($yid)
+            ->getQuery()
+            ->getSingleResult();
+
+        if (!$yt) {
+            return null;
+        }
+
+        return $this->mmobjRepo->find($yt->getMultimediaObjectId());
+    }
+
+
+    private function getSeries($seriesId)
+    {
+        if (!$seriesId) {
+            throw new \Exception('No series id argument');
+        }
+        $series = $this->seriesRepo->find($seriesId);
+        if (!$series) {
+            throw new \Exception('No series with id '. $seriesId);
+        }
+        return $series;
+    }
+
+    private function initParameters()
+    {
+        $this->dm = $this->getContainer()->get('doctrine_mongodb')->getManager();
+        $this->tagRepo = $this->dm->getRepository('PumukitSchemaBundle:Tag');
+        $this->mmobjRepo = $this->dm->getRepository('PumukitSchemaBundle:MultimediaObject');
+        $this->seriesRepo = $this->dm->getRepository('PumukitSchemaBundle:Series');
+        $this->youtubeRepo = $this->dm->getRepository('PumukitYoutubeBundle:Youtube');
+
+        $this->youtubeService = $this->getContainer()->get('pumukityoutube.youtube');
+        $this->factoryService = $this->getContainer()->get('pumukitschema.factory');
+
+        $this->logger = $this->getContainer()->get('monolog.logger.youtube');
+    }
+}
