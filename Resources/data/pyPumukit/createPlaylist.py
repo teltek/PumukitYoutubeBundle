33d04d09c8bab3b4ec8d4b3823b33d43d76b0485
rubenrua <rubenrua@teltek.es>
2017-09-01 11:18:01 +0200

    TTK-15140 python multiaccount

--- Resources/data/pyPumukit/createPlaylist.py
+++ Resources/data/pyPumukit/createPlaylist.py
@@ -1,79 +1,21 @@
 #!/usr/bin/python
 
-import httplib2_monkey_patch
+import httplib2_monkey_patch  # noqa: F401
 
-import httplib
-import httplib2
-import os
 import sys
 import json
 
-from apiclient.discovery import build
 from apiclient.errors import HttpError
-from oauth2client.file import Storage
-from oauth2client.client import flow_from_clientsecrets
-from oauth2client.tools import run_flow
-from oauth2client.tools import argparser
 from optparse import OptionParser
-from pprint import pprint
 
-
-# CLIENT_SECRETS_FILE, name of a file containing the OAuth 2.0 information for
-# this application, including client_id and client_secret. You can acquire an
-# ID/secret pair from the API Access tab on the Google APIs Console
-#   http://code.google.com/apis/console#access
-# For more information about using OAuth2 to access Google APIs, please visit:
-#   https://developers.google.com/accounts/docs/OAuth2
-# For more information about the client_secrets.json file format, please visit:
-#   https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
-# Please ensure that you have enabled the YouTube Data API for your project.
-CLIENT_SECRETS_FILE = "client_secrets.json"
-
-# Helpful message to display if the CLIENT_SECRETS_FILE is missing.
-MISSING_CLIENT_SECRETS_MESSAGE = """
-WARNING: Please configure OAuth 2.0
-
-To make this sample run you will need to populate the client_secrets.json file
-found at:
-
-   %s
-
-with information from the APIs Console
-https://code.google.com/apis/console#access
-
-For more information about the client_secrets.json file format, please visit:
-https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
-""" % os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                   CLIENT_SECRETS_FILE))
-
-# A limited OAuth 2 access scope that allows for read-only access.
-YOUTUBE_SCOPE = "https://www.googleapis.com/auth/youtube"
-YOUTUBE_API_SERVICE_NAME = "youtube"
-YOUTUBE_API_VERSION = "v3"
-
-def get_authenticated_service():
-  flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE,
-                                 message=MISSING_CLIENT_SECRETS_MESSAGE,
-                                 scope=YOUTUBE_SCOPE)
-
-  storage = Storage("pumukit-oauth2.json")
-  credentials = storage.get()
-
-  if credentials is None or credentials.invalid:
-    print('No credentials, running authentication flow to get OAuth token')
-    flags = argparser.parse_args(args=['--noauth_local_webserver'])
-    credentials = run_flow(flow, storage, flags)
-
-
-  return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-                 http=credentials.authorize(httplib2.Http()))
+from py_pumukit_lib import get_authenticated_service
 
 
 def create_playlist(options):
   out = {'error': False, 'out': None}
 
   try:
-    youtube = get_authenticated_service()
+    youtube = get_authenticated_service(options.account)
     playlists_insert_response = youtube.playlists().insert(
       part="snippet,status",
       body=dict(
@@ -108,17 +50,14 @@ def create_playlist(options):
     return -1
 
 
-
-
-
-
-
 if __name__ == '__main__':
   parser = OptionParser()
   parser.add_option("--title", dest="title",
     help="Title for the playlist.")
   parser.add_option("--privacyStatus", dest="privacyStatus",
     help="Privacy status for the playlist.")
+  parser.add_option("--account", dest="account",
+    help="Youtube account id.")
 
   (options, args) = parser.parse_args()
 
--- Resources/data/pyPumukit/deletePlaylist.py
+++ Resources/data/pyPumukit/deletePlaylist.py
@@ -1,78 +1,20 @@
 #!/usr/bin/python
 
-import httplib2_monkey_patch
+import httplib2_monkey_patch  # noqa: F401
 
-import httplib2
-import os
-import random
 import sys
-import time
 import json
 
-from apiclient.discovery import build
-from apiclient.errors import HttpError
-from oauth2client.file import Storage
-from oauth2client.client import flow_from_clientsecrets
-from oauth2client.tools import run_flow
-from oauth2client.tools import argparser
 from optparse import OptionParser
-from pprint import pprint
 
+from py_pumukit_lib import get_authenticated_service
 
-
-# CLIENT_SECRETS_FILE, name of a file containing the OAuth 2.0 information for
-# this application, including client_id and client_secret. You can acquire an
-# ID/secret pair from the API Access tab on the Google APIs Console
-#   http://code.google.com/apis/console#access
-# For more information about using OAuth2 to access Google APIs, please visit:
-#   https://developers.google.com/accounts/docs/OAuth2
-# For more information about the client_secrets.json file format, please visit:
-#   https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
-# Please ensure that you have enabled the YouTube Data API for your project.
-CLIENT_SECRETS_FILE = "client_secrets.json"
-
-# An OAuth 2 access scope that allows for full read/write access.
-YOUTUBE_SCOPE = "https://www.googleapis.com/auth/youtube"
-YOUTUBE_API_SERVICE_NAME = "youtube"
-YOUTUBE_API_VERSION = "v3"
-
-# Helpful message to display if the CLIENT_SECRETS_FILE is missing.
-MISSING_CLIENT_SECRETS_MESSAGE = """
-WARNING: Please configure OAuth 2.0
-
-To make this sample run you will need to populate the client_secrets.json file
-found at:
-
-   %s
-
-with information from the APIs Console
-https://code.google.com/apis/console#access
-
-For more information about the client_secrets.json file format, please visit:
-https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
-""" % os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                   CLIENT_SECRETS_FILE))
-
-def get_authenticated_service(id):
-  flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE, scope=YOUTUBE_SCOPE,
-    message=MISSING_CLIENT_SECRETS_MESSAGE)
-
-  storage = Storage("pumukit-oauth%s.json" % id)
-  credentials = storage.get()
-
-  if credentials is None or credentials.invalid:
-    print('No credentials, running authentication flow to get OAuth token')
-    flags = argparser.parse_args(args=['--noauth_local_webserver'])
-    credentials = run_flow(flow, storage, flags)
-
-  return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-    http=credentials.authorize(httplib2.Http()))
 
 def delete_playlist(options):
   out = {'error': False, 'out': None}
 
   try:
-    youtube = get_authenticated_service(options.ytid)
+    youtube = get_authenticated_service(options.account)
     playlists_list_response = youtube.playlists().list(
       id=options.playlistid,
       part='snippet'
@@ -84,7 +26,7 @@ def delete_playlist(options):
       print json.dumps(out)
       return -1
 
-    playlist = playlists_list_response["items"][0]
+    playlists_list_response["items"][0]
 
     out['out'] = youtube.playlists().delete(id=options.playlistid).execute()
 
@@ -101,14 +43,12 @@ if __name__ == "__main__":
   parser = OptionParser()
   parser.add_option("--playlistid", dest="playlistid",
     help="ID of playlist to delete.")
-  parser.add_option("--ytid", dest="ytid",
+  parser.add_option("--account", dest="account",
     help="Youtube account id.")
 
   (options, args) = parser.parse_args()
 
   if options.playlistid is None:
    exit("Please specify a valid playlist using --playlistid= parameter")
-  if options.ytid is None:
-    options.ytid = "2"
 
   delete_playlist(options)
