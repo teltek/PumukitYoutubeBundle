6b97986282ae2dc4acefe1a2a3fa55c8a52c9f71
Pablo Ogando Ferreira <pablo.ogando@teltek.es>
2024-03-08 11:30:33 +0100

    TTK-26902 fix namespaces

--- Command/ImportJobsFromYouTubeDownloadCommand.php
+++ Command/ImportJobsFromYouTubeDownloadCommand.php
@@ -10,6 +10,7 @@ use Pumukit\SchemaBundle\Document\MultimediaObject;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 final class ImportJobsFromYouTubeDownloadCommand extends Command
